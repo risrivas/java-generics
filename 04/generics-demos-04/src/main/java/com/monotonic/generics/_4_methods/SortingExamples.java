d08991a69a9d554b4b9734ca7999209cabf8388c
Rishi Srivastava <rishisrivasta@gmail.com>
2021-08-16 07:30:36 +0800

    generics on methods

--- 04/generics-demos-04/src/main/java/com/monotonic/generics/_4_methods/SortingExamples.java
+++ 04/generics-demos-04/src/main/java/com/monotonic/generics/_4_methods/SortingExamples.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class SortingExamples
-{
-    public static void main(String[] args)
-    {
+public class SortingExamples {
+    public static void main(String[] args) {
         Person donDraper = new Person("Don Draper", 89);
         Person peggyOlson = new Person("Peggy Olson", 65);
         Person bertCooper = new Person("Bert Cooper", 100);
@@ -22,20 +20,16 @@ public class SortingExamples
         System.out.println(youngestCastMember);
     }
 
-    public static <T> T min(List<T> values, Comparator<T> comparator)
-    {
-        if (values.isEmpty())
-        {
+    public static <T> T min(List<T> values, Comparator<T> comparator) {
+        if (values == null || values.isEmpty()) {
             throw new IllegalArgumentException("Unable to find the minimum of an empty list");
         }
 
         T lowestFound = values.get(0);
 
-        for (int i = 1; i < values.size(); i++)
-        {
+        for (int i = 1; i < values.size(); i++) {
             final T element = values.get(i);
-            if (comparator.compare(element, lowestFound) < 0)
-            {
+            if (comparator.compare(element, lowestFound) < 0) {
                 lowestFound = element;
             }
         }
